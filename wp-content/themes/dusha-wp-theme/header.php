85e0b41292da45818b56ddcb555c51eb6119b935
andriisukhoi <andrii.sukhoi@gmail.com>
2024-04-04 08:45:10 +0200

    style page template

--- wp-content/themes/dusha-wp-theme/header.php
+++ wp-content/themes/dusha-wp-theme/header.php
@@ -22,7 +22,7 @@
 
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
-<div id="page" class="site">
+<div id="page" class="site bg-black">
     <a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'dusha-wp-theme' ); ?></a>
 
 	<header id="masthead" class="site-header">
--- wp-content/themes/dusha-wp-theme/page.php
+++ wp-content/themes/dusha-wp-theme/page.php
@@ -13,10 +13,11 @@
  */
 
 get_header();
+global $post;
 ?>
-
-	<div id="primary" class="container">
-		<?php global $post;?>
+	<div id="primary" class="container color-white">
+        <h1 class="color-primary page-content-title mt-5"><?=$post->post_title?></h1>
+        <?=$post->post_content;?>
 	</div><!-- #main -->
 
 <?php
