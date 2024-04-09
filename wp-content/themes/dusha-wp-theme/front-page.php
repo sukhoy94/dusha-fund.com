d3d5de787c9c5616ae76689871afd4000ac4b25e
andriisukhoi <andrii.sukhoi@gmail.com>
2024-04-09 18:21:44 +0200

    Remove content from front-page, only logo stays

--- wp-content/themes/dusha-wp-theme/front-page.php
+++ wp-content/themes/dusha-wp-theme/front-page.php
@@ -14,22 +14,6 @@
         <div class="row vh-md-100">
             <div class="col-md-8 col-sm-10 col-12 mx-auto my-auto text-center color-white">
                 <?php require ('template-parts/logo-svg.php'); ?>
-
-                <?php
-                $query = new WP_Query([
-                    'category_name' => 'strona-w-budowie-informacja',
-                ]);
-                if ( $query->have_posts() ) :
-                    ?>
-                    <?php while ( $query->have_posts() ) : $query->the_post(); ?>
-                    <h2 class="mt-3"><?php the_title(); ?></h2>
-                <?php endwhile; ?>
-                    <?php wp_reset_postdata(); ?>
-                <?php endif; ?>
-
-                <a class="btn btn-primary dusha-btn-frontpage" target="_blank" href="https://dusha-project.com">
-                     Przejdź do projektu Dusha moja - Ukraina
-                </a>
             </div>
         </div>
     </div>
