b6d2e45bd6c9ebad7419b8c2d8819faaa0989cf5
andriisukhoi <andrii.sukhoi@gmail.com>
2024-04-13 14:10:35 +0200

    Create a hero section text on a front-page.php

--- wp-content/themes/dusha-wp-theme/front-page.php
+++ wp-content/themes/dusha-wp-theme/front-page.php
@@ -2,18 +2,24 @@
 
     <?php
         $query = new WP_Query([
-            'category_name' => 'strona-w-budowie-informacja',
+            'category_name' => 'front-page-hero-section',
+            'posts_per_page' => 1,
         ]);
+
+        $post = $query->posts[0];
     ?>
-
-
-
-
 <section class="front-page-hero height-100 bg-black d-flex align-items-center" id="home">
     <div class="container hero-text-container">
         <div class="row vh-md-100">
             <div class="col-md-8 col-sm-10 col-12 mx-auto my-auto text-center color-white">
-                <?php require ('template-parts/logo-svg.php'); ?>
+                <?php if ($post !== null): ?>
+                <h1><?=$post->post_title;?></h1>
+                <div class="py-2">
+                    <?=$post->post_content;?>
+                </div>
+                <?php else: ?>
+                    <?php require ('template-parts/logo-svg.php'); ?>
+                <?php endif;?>
             </div>
         </div>
     </div>
