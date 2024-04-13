29a478954e4e9363d3dac0b4c6f02b9976494594
andriisukhoi <andrii.sukhoi@gmail.com>
2024-04-13 13:16:29 +0200

    Remove comments from header wp_nav_menu

--- wp-content/themes/dusha-wp-theme/header.php
+++ wp-content/themes/dusha-wp-theme/header.php
@@ -39,21 +39,15 @@
 
                 <?php
                 wp_nav_menu( array(
-                    'menu'            => '',              // (string) Название выводимого меню (указывается в админке при создании меню, приоритетнее
-                    'container'       => 'div',           // (string) Контейнер меню. Обворачиватель ul. Указывается тег контейнера (по умолчанию в тег div)
-                    'container_class' => 'collapse justify-content-end navbar-collapse',              // (string) class контейнера (div тега)
-                    'container_id'    => 'navbarSupportedContent',              // (string) id контейнера (div тега)
-                    'menu_class'      => 'navbar-nav',          // (string) class самого меню (ul тега)
-                    'menu_id'         => '',              // (string) id самого меню (ul тега)
-                    'echo'            => true,            // (boolean) Выводить на экран или возвращать для обработки
-                    'fallback_cb'     => 'wp_page_menu',  // (string) Используемая (резервная) функция, если меню не существует (не удалось получить)
-                    'before'          => '',              // (string) Текст перед <a> каждой ссылки
-                    'after'           => '',              // (string) Текст после </a> каждой ссылки
-                    'link_before'     => '',              // (string) Текст перед анкором (текстом) ссылки
-                    'link_after'      => '<i class="fa-solid fa-arrow-up-right-from-square"></i>',              // (string) Текст после анкора (текста) ссылки
-                    'depth'           => 0,               // (integer) Глубина вложенности (0 - неограничена, 2 - двухуровневое меню)
-                    'walker'          => '',              // (object) Класс собирающий меню. Default: new Walker_Nav_Menu
-                    'theme_location'  => 'top-menu',               // (string) Расположение меню в шаблоне. (указывается ключ которым было зарегистрировано меню в функции register_nav_menus)
+                    'menu'            => '',
+                    'container'       => 'div',
+                    'container_class' => 'collapse justify-content-end navbar-collapse',
+                    'container_id'    => 'navbarSupportedContent',
+                    'menu_class'      => 'navbar-nav',
+                    'echo'            => true,
+                    'fallback_cb'     => 'wp_page_menu',
+                    'link_after'      => '<i class="fa-solid fa-arrow-up-right-from-square"></i>',
+                    'theme_location'  => 'top-menu',
                 ) );
                 ?>
             </div>
