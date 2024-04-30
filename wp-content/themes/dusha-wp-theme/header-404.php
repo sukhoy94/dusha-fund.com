74f87e403100eb1cf47bc4aa90368d1d7544f72e
andriisukhoi <andrii.sukhoi@gmail.com>
2024-04-30 20:18:42 +0200

    Style custom 404 page template

---
+++ wp-content/themes/dusha-wp-theme/header-404.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="https://gmpg.org/xfn/11">
+
+    <?php wp_head(); ?>
+
+    <script src="https://kit.fontawesome.com/5ae6764514.js" crossorigin="anonymous"></script>
+</head>
+
+<body class="bg-black">
