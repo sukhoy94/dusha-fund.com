fa9064d53ec9cc000481d1ff0da8828848b60bc6
andriisukhoi <andrii.sukhoi@gmail.com>
2024-05-02 19:39:31 +0200

    Stick footer to the bottom of the page

--- wp-content/themes/dusha-wp-theme/page.php
+++ wp-content/themes/dusha-wp-theme/page.php
@@ -16,7 +16,7 @@ get_header();
 global $post;
 ?>
 
-	<div id="primary" class="container color-white">
+	<div id="primary" class="container color-white pb-9rem">
         <h1 class="color-primary page-content-title mt-5"><?=$post->post_title?></h1>
         <?=$post->post_content;?>
 	</div><!-- #main -->
