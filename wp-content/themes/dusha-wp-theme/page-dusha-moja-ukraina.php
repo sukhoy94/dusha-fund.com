343f4722c8f8ba1941f4d81ec083c8119556a0dc
andriisukhoi <andrii.sukhoi@gmail.com>
2024-06-15 20:00:46 +0200

    add hero section

--- wp-content/themes/dusha-wp-theme/page-dusha-moja-ukraina.php
+++ wp-content/themes/dusha-wp-theme/page-dusha-moja-ukraina.php
@@ -1,4 +1,15 @@
 <?php /* Template Name: Dusha moja - Ukraina project */ ?>
 <?php get_header();?>
-
+<section class="bg-hero d-flex align-items-center" id="home">
+    <div class="container hero-text-container">
+        <div class="row vh-md-100">
+            <div class="col-md-8 col-sm-10 col-12 mx-auto my-auto text-center">
+                <h1 class="heading-black text-capitalize">
+                    Душа моя - Україна
+                </h1>
+                <p class="lead py-3">Розвиток культури, інтеграція в європейське суспільство через мистецтво та обʼєднання</p>
+            </div>
+        </div>
+    </div>
+</section>
 <?php get_footer();?>
