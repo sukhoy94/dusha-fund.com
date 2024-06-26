3bb82cc9cd12daffe9cebb12b2ce3eaf8d818867
andriisukhoi <andrii.sukhoi@gmail.com>
2024-06-26 21:36:26 +0200

    move mission to separate file

---
+++ wp-content/themes/dusha-wp-theme/inc/dusha-moja-ukraina/mission.php
@@ -0,0 +1,82 @@
+<!-- mission start -->
+<section class="section-angle top-left bottom-left pb-5 pt-5 mission-wrapper" id="charity">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-9 mx-auto text-center">
+                <h2 class="heading-black color-pink">
+                    Місія
+                </h2>
+                <p class="mission-description">
+
+                    Проєкт "Душа моя - Україна" має на меті об'єднати молодь яка прагне підтримати Україну в складний
+                    період її історії. Ми віримо, що мистецтво має силу з'єднати серця та душі людей, тому
+                    використовуємо
+                    його як інструмент, щоб нагадати світові про війну в Україні та підтримку її майбутнього.
+
+                </p>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="mx-auto mb-3">
+                <ul class="mission-tags">
+                    <li>#dusha</li>
+                    <li>#duszamojaUkraina</li>
+                    <li>#dushaoncanvas</li>
+                    <li>#dushaonplener</li>
+                    <li>#dushateams</li>
+                    <li>#dushainproсes</li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-12 mission-type mb-3">
+                <div class="row">
+                    <div class="mission-slogan col-lg-3 col-md-4 d-flex align-self-center">
+                        <h4 class="color-pink mb-0">
+                            Об’єднуємо
+                        </h4>
+                    </div>
+                    <div class="mission-slogan-content col-md-8 col-lg-9 d-flex">
+                        <p class="mb-0">
+                            ініціативну молодь задля спільної мети, даючи кожному простір на розвиток своїх здібностей
+                            та талантів.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-12 mission-type mb-3">
+                <div class="row">
+                    <div class="mission-slogan col-lg-3 col-md-4 d-flex align-self-center">
+                        <h4 class="color-pink mb-0">
+                            Підтримуємо
+                        </h4>
+                    </div>
+                    <div class="mission-slogan-content col-md-8 col-lg-9 d-flex">
+                        <p class="mb-0">
+                            культуру, плекаючи всім відоме, додаючи до нього своє та відкриваючи нове.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-12 mission-type mb-3">
+                <div class="row">
+                    <div class="mission-slogan col-lg-3 col-md-4 d-flex align-self-center">
+                        <h4 class="color-pink mb-0">
+                            Допомагаємо
+                        </h4>
+                    </div>
+                    <div class="mission-slogan-content col-md-8 col-lg-9 d-flex">
+                        <p class="mb-0">
+                            постраждалим від війни в Україні. Нагадуємо світові про війну, що триває до сьогодні, і
+                            людей, що досі потребують нашої допомоги.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
+<!-- mission stop -->
