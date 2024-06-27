ff0ec58505dc000d848ca196d25ce6fadcff610e
andriisukhoi <andrii.sukhoi@gmail.com>
2024-06-27 16:52:09 +0200

    add slogan part

--- wp-content/themes/dusha-wp-theme/backend/translations/polylang.php
+++ wp-content/themes/dusha-wp-theme/backend/translations/polylang.php
@@ -14,3 +14,9 @@ pll_register_string('dusha_hero_subtitle', 'Hero subtytul', 'Theme Strings');
 // mission
 pll_register_string('dusha_mission_title', 'Mission Title', 'Theme Strings');
 pll_register_string('dusha_mission_subtitle', 'Mission Subtitle', 'Theme Strings');
+pll_register_string('dusha_mission_slogan1_title', 'Mission Slogan1 Title', 'Theme Strings');
+pll_register_string('dusha_mission_slogan1_title', 'Mission Slogan2 Title', 'Theme Strings');
+pll_register_string('dusha_mission_slogan1_title', 'Mission Slogan3 Title', 'Theme Strings');
+pll_register_string('dusha_mission_slogan1_title', 'Mission SloganDescription1', 'Theme Strings');
+pll_register_string('dusha_mission_slogan1_title', 'Mission SloganDescription2', 'Theme Strings');
+pll_register_string('dusha_mission_slogan1_title', 'Mission SloganDescription3', 'Theme Strings');
