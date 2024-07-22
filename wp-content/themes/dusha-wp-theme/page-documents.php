03b290a7cc4d5e70337b4e249cee48728800b1aa
andriisukhoi <andrii.sukhoi@gmail.com>
2024-07-22 15:06:04 +0200

    add padding bottom to documents page main wrapper

--- wp-content/themes/dusha-wp-theme/page-documents.php
+++ wp-content/themes/dusha-wp-theme/page-documents.php
@@ -7,7 +7,7 @@
     setup_postdata($page);
 ?>
 
-<div class="container">
+<div class="container pb-9rem">
     <div>
         <h1 class="color-white documents-page-title">
             <?php the_title(); ?>
@@ -15,7 +15,7 @@
     </div>
 
     <div class="row d-flex mt-5">
-        <div class="text-center col-md-4">
+        <div class="text-center col-md-4 mb-3">
             <div class="card document-card">
                 <a 
                     href="<?php echo get_template_directory_uri() . '/assets/docs/statut_web.pdf'?>"
@@ -38,7 +38,7 @@
                 </div>
             </div>            
         </div>
-        <div class="text-center col-md-4">
+        <div class="text-center col-md-4 mb-3">
             <div class="card document-card">
                 <a 
                     href="<?php echo get_template_directory_uri() . '/assets/docs/standardy_ochrony_maloletnich.pdf'; ?>"
