749bc3533fddff591c13a4243b12dc15c6dd88fd
andriisukhoi <andrii.sukhoi@gmail.com>
2024-07-25 13:46:48 +0200

    remove displaying posts on front page

--- wp-content/themes/dusha-wp-theme/front-page.php
+++ wp-content/themes/dusha-wp-theme/front-page.php
@@ -1,27 +1,11 @@
 <?php get_header();?>
 
-    <?php
-        $query = new WP_Query([
-            'category_name' => 'front-page-hero-section',
-            'posts_per_page' => 1,
-        ]);
-        
-        if (!empty($query->posts)) {
-            $post = $query->posts[0];
-        }
-    ?>
+
 <section class="front-page-hero d-flex align-items-center" id="home">
     <div class="container hero-text-container">
         <div class="row vh-md-100">
             <div class="col-md-8 col-sm-10 col-12 mx-auto my-auto text-center color-white">
-                <?php if ($post !== null): ?>
-                <h1><?=$post->post_title;?></h1>
-                <div class="py-2">
-                    <?=$post->post_content;?>
-                </div>
-                <?php else: ?>
-                    <?php require ('template-parts/logo-svg.php'); ?>
-                <?php endif;?>
+                <?php require ('template-parts/logo-svg.php'); ?>
             </div>
         </div>
     </div>
