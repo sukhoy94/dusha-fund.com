5c394ea8f815fd203f22cff502980f545e64ed40
andriisukhoi <andrii.sukhoi@gmail.com>
2024-07-25 19:06:43 +0200

    Change a text when there are no posts in blog

--- wp-content/themes/dusha-wp-theme/category-blog.php
+++ wp-content/themes/dusha-wp-theme/category-blog.php
@@ -45,7 +45,7 @@
         </nav>
         
         <?php else : ?>
-            <p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
+            <p><?php esc_html_e( 'Uszanowanko, nic tu jeszcze nie ma...' ); ?></p>
         <?php endif; ?>     
     </div>
 </div>
